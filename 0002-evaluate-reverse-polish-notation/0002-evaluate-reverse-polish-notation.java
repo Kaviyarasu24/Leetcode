6550cef11a5e6058fa06ded847ec24b609f99ddf
Kaviyarasu P <152471766+Kaviyarasu24@users.noreply.github.com>
2026-04-09 14:52:39 +0530

    04-09-2026 0002-evaluate-reverse-polish-notation Medium .java

---
+++ 0002-evaluate-reverse-polish-notation/0002-evaluate-reverse-polish-notation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            if ("+-*/".contains(token)) {
+                int a = stack.pop();
+                int b = stack.pop();
+                switch (token) {
+                    case "+": stack.push(b + a); break;
+                    case "-": stack.push(b - a); break;
+                    case "*": stack.push(b * a); break;
+                    case "/": stack.push(b / a); break; 
+                }
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.peek();
+    }
+
+}
